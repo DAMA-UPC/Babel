ef6605107c4e97ffb96eddc4eacd65d45be0b05d
Xavier Fernández Salas <xavier.fernandez.salas@gmail.com>
2016-12-28 10:08:26 +0100

    Add container for the generated graphs

---
+++ src/main/scala/edu/upc/dama/models/PropertyGraph.scala
@@ -0,0 +1,13 @@
+package edu.upc.dama.models
+
+import cats.data.NonEmptyStream
+
+/**
+  * Represents a generated graph.
+  *
+  * @param vertices of the generated graph.
+  * @param edges    of the generated graph.
+  * @tparam ID of the generated [[Vertex]] unique identifiers.
+  */
+case class PropertyGraph[ID](vertices: NonEmptyStream[Vertex[ID]],
+                             edges: NonEmptyStream[Edge[ID]])
