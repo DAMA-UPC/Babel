638b1ca8e0f69c3623d5e42d0e254789232fa1ed
Xavier Fernández Salas <xavier.fernandez.salas@gmail.com>
2016-12-28 10:01:50 +0100

    Add Scalacheck arbitrary generator for 'java.util.UUID'

---
+++ src/test/scala/edu/upc/dama/TestUtils.scala
@@ -0,0 +1,20 @@
+package edu.upc.dama
+
+import java.util.UUID
+
+import org.scalacheck.{Arbitrary, Gen}
+
+/**
+  * Set of utilities for Unit testing.
+  */
+object TestUtils {
+
+  /**
+    * Scalacheck arbitrary function for [[java.util.UUID]].
+    */
+  implicit def arbUUID: Arbitrary[UUID] = {
+    Arbitrary(
+      Gen.delay(UUID.randomUUID)
+    )
+  }
+}
