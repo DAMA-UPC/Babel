8fb30529314eaf31dd2995fb93b886ac8784f122
Xavier Fernández Salas <xavier.fernandez.salas@gmail.com>
2017-01-28 15:19:54 +0100

    Improve 'Class2Map' macro annotation reusability

--- macros/src/main/scala/macros/Class2Map.scala
+++ macros/src/main/scala/macros/Class2Map.scala
@@ -2,6 +2,7 @@ package macros
 
 import scala.annotation.compileTimeOnly
 import scala.collection.immutable.Seq
+import scala.meta.Term.Param
 import scala.meta._
 
 /**
@@ -20,21 +21,27 @@ import scala.meta._
   */
 @compileTimeOnly("@Class2Map not expanded")
 class Class2Map extends scala.annotation.StaticAnnotation {
-  inline def apply(defn: Any): Any = meta {
-    defn match {
-      case cls @ Defn.Class(_, _, _, Ctor.Primary(_, _, paramss), template) =>
-        val namesToValues: Seq[Term.Tuple] = paramss.flatten.map { param =>
-          q"(${param.name.syntax}, ${Term.Name(param.name.value)})"
-        }
-        val toMapImpl: Term =
-          q"_root_.scala.collection.Map[String, Any](..$namesToValues)"
-        val toMap =
-          q"def toMap: _root_.scala.collection.Map[String, Any] = $toMapImpl"
-        val templateStats: Seq[Stat] = toMap +: template.stats.getOrElse(Nil)
-        cls.copy(templ = template.copy(stats = Some(templateStats)))
-      case _ =>
-        println(defn.structure)
-        abort("@Class2Map must annotate a class.")
-    }
+  inline def apply(defn: Any): Any = meta(Class2Map.impl(defn))
+}
+
+object Class2Map {
+
+  def impl(defn: Stat): Stat = {
+      defn match {
+        case cls@Defn.Class(_, _, _, Ctor.Primary(_, _, paramss), template) =>
+          val namesToValues: Seq[Term.Tuple] = paramss.flatten.map {
+            (param: Param) =>
+              q"(${param.name.syntax}, ${Term.Name(param.name.value)})"
+          }
+          val toMapImpl: Term =
+            q"_root_.scala.collection.Map[String, Any](..$namesToValues)"
+          val toMap =
+            q"def toMap: _root_.scala.collection.Map[String, Any] = $toMapImpl"
+          val templateStats: Seq[Stat] = toMap +: template.stats.getOrElse(Nil)
+          cls.copy(templ = template.copy(stats = Some(templateStats)))
+        case _ =>
+          println(defn.structure)
+          abort("@Class2Map must annotate a class.")
+      }
   }
 }
