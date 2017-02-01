fb664f4163b7b87e29c9fcca10e558b61d89d00e
Xavier Fernández Salas <xavier.fernandez.salas@gmail.com>
2017-02-01 16:22:16 +0100

    Implement '@FromMapApply' macro annotation

---
+++ schemas/src/main/scala/macros/FromMapApply.scala
@@ -0,0 +1,79 @@
+package macros
+
+import scala.collection.immutable.Seq
+import scala.meta.Term.Block
+import scala.meta._
+
+/**
+  * Before:
+  * {{{
+  * @FromMapApply
+  * case class Test(a: Int, b: String, c: Float)
+  * }}}
+  *
+  * After:
+  *
+  * {{{
+  * object Test {
+  * def apply(m: _root_.scala.collection.Map[String, Any]): Test = {
+  *  new Test(m("a").asInstanceOf[Int], m("b").asInstanceOf[String], m("c").asInstanceOf[Float])
+  * }
+  * }}}
+  */
+class FromMapApply extends scala.annotation.StaticAnnotation {
+  inline def apply(defn: Any): Any = meta(FromMapApply.impl(defn))
+}
+
+/**
+  * Object containing the [[FromMapApply]] macro annotation expansion implementation.
+  */
+object FromMapApply {
+
+  /**
+    * Implementation of the [[FromMapApply]] macro expansion.
+    */
+  private[macros] val impl: (Stat) => Block = {
+    (defn: Stat) => {
+      defn match {
+        case Term.Block(Seq(cls@Defn.Class(_, name, _, ctor, _), companion: Defn.Object)) =>
+          // companion object exists
+          val applyMethod = createApply(name, ctor.paramss)
+          val templateStats: Seq[Stat] =
+            applyMethod +: companion.templ.stats.getOrElse(Nil)
+          val newCompanion = companion.copy(
+            templ = companion.templ.copy(stats = Some(templateStats)))
+          Term.Block(Seq(cls, newCompanion))
+        case cls@Defn.Class(_, name, _, ctor, _) =>
+          // companion object does not exists
+          val applyMethod = createApply(name, ctor.paramss)
+          val companion = q"object ${Term.Name(name.value)} { $applyMethod }"
+          Term.Block(Seq(cls, companion))
+        case _ =>
+          println(defn.structure)
+          abort("@FromMapApply must annotate a class.")
+      }
+    }
+  }
+
+  /**
+    * Generates the apply method with all the required assignations.
+    */
+  private[this] def createApply(name: Type.Name, paramss: Seq[Seq[Term.Param]]): Defn.Def = {
+
+    def argAssignations: Seq[Seq[Term.Name]] = {
+      paramss.map(_.map(
+        (param: Term.Param) => {
+          val argName = Term.Name(param.name.syntax)
+          val quotedArgName = Term.Name("\"" + param.name.syntax + "\"")
+          val termType = Type.Name(param.decltpe.map(_.toString()).getOrElse("Any"))
+          Term.Name(q"m($quotedArgName).asInstanceOf[$termType]".syntax)
+        }
+      )
+      )
+    }
+    q"""def apply(m : _root_.scala.collection.Map[String, Any]): $name = {
+            new ${Ctor.Ref.Name(name.value)}(...$argAssignations)
+        }
+        """
+  }
+}
---
+++ schemas/src/test/scala/macros/FromMapApplySpec.scala
@@ -0,0 +1,39 @@
+package macros
+
+import org.specs2.ScalaCheck
+import org.specs2.mutable.Specification
+
+/**
+  * Test the macro [[FromMapApply]].
+  */
+class FromMapApplySpec extends Specification with ScalaCheck {
+
+  "@Macro annotation expansion" should {
+    "work with single parameter classes" in {
+      @FromMapApply case class Test(value: Int)
+      prop {
+        (expectedValue: Int) =>
+          Test.apply(Map("value" -> expectedValue)) must beEqualTo(Test(expectedValue))
+      }
+    }
+    "work with multiple parameter classes" in {
+      @FromMapApply case class Test(intValue: Int, stringValue: String, floatValue: Float)
+      prop {
+        (intValue: Int, stringValue: String, floatValue: Float) =>
+          Test(
+            intValue = intValue,
+            stringValue = stringValue,
+            floatValue = floatValue
+          ) must beEqualTo(
+            Test.apply(
+              Map(
+                "intValue" -> intValue,
+                "stringValue" -> stringValue,
+                "floatValue" -> floatValue
+              )
+            )
+          )
+      }
+    }
+  }
+}
