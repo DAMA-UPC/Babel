89b2fb4f0f24429e0b61ca988d70be4f49a520fa
Xavier Fernández Salas <xavier.fernandez.salas@gmail.com>
2017-03-17 12:48:35 +0100

    Document 'NumericTypeConstraint' default constraints

--- types/src/main/scala/types/primitives/numeric/NumericTypesConstraints.scala
+++ types/src/main/scala/types/primitives/numeric/NumericTypesConstraints.scala
@@ -13,12 +13,21 @@ sealed case class NumericTypeConstraint private (name: String,
   */
 trait NumericTypesConstraints {
 
+  /**
+    * Sets the minimum possible value of a Number.
+    */
   def minValue(value : Number) : NumericTypeConstraint =
     NumericTypeConstraint("MinValue", value)
 
+  /**
+    * Sets the minimum possible value of a Number.
+    */
   def maxValue(value: Number) : NumericTypeConstraint =
     NumericTypeConstraint("MaxValue", value)
 
+  /**
+    * Sets the maximum number of decimals of a Number.
+    */
   def maxNumberDecimals(value: Number) : NumericTypeConstraint =
     NumericTypeConstraint("MaxNumberDecimals", value)
 }
