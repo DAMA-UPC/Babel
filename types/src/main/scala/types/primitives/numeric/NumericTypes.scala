986c9a875b243f4485d1d2a101c24ece733d914e
Xavier Fernández Salas <xavier.fernandez.salas@gmail.com>
2017-03-17 11:09:30 +0100

    Fix some Codacy issues

--- types/src/main/scala/types/primitives/numeric/NumericTypes.scala
+++ types/src/main/scala/types/primitives/numeric/NumericTypes.scala
@@ -11,7 +11,7 @@ trait NumericTypes {
   /**
     * Represents a 8 bit signed integer on the Babel AST.
     */
-  private[types] val BYTE: NumericType =
+  private[types] val astByte: NumericType =
     NumericType(
       minValue(Byte.MinValue),
       maxValue(Byte.MaxValue),
@@ -19,14 +19,14 @@ trait NumericTypes {
     )
 
   /**
-    * Implicit conversion from [[scala.Byte]] to [[BYTE]].
+    * Implicit conversion from [[scala.Byte]] to [[astByte]].
     */
-  implicit def byteToBabelByte(typ: Byte.type): NumericType = BYTE
+  implicit def byteToBabelByte(typ: Byte.type): NumericType = astByte
 
   /**
     * Represents a 16 bit signed integer on the Babel AST.
     */
-  private[types] val SHORT: NumericType =
+  private[types] val astShort: NumericType =
     NumericType(
       minValue(Short.MinValue),
       maxValue(Short.MaxValue),
@@ -34,14 +34,14 @@ trait NumericTypes {
     )
 
   /**
-    * Implicit conversion from [[scala.Short]] to [[SHORT]].
+    * Implicit conversion from [[scala.Short]] to [[astShort]].
     */
-  implicit def shortToBabelShort(typ: Short.type): NumericType = SHORT
+  implicit def shortToBabelShort(typ: Short.type): NumericType = astShort
 
   /**
     * Represents a 32 bit signed integer on the Babel AST.
     */
-  private[types] val INTEGER: NumericType =
+  private[types] val astInt: NumericType =
     NumericType(
       minValue(Int.MinValue),
       maxValue(Int.MaxValue),
@@ -49,14 +49,14 @@ trait NumericTypes {
     )
 
   /**
-    * Implicit conversion from [[scala.Int]] to [[INTEGER]].
+    * Implicit conversion from [[scala.Int]] to [[astInt]].
     */
-  implicit def intToBabelInteger(typ: Int.type): NumericType = INTEGER
+  implicit def intToBabelInteger(typ: Int.type): NumericType = astInt
 
   /**
     * Represents a 64 bit signed integer on the Babel AST.
     */
-  private[types] val LONG: NumericType =
+  private[types] val astLong: NumericType =
     NumericType(
       minValue(Long.MinValue),
       maxValue(Long.MaxValue),
@@ -64,27 +64,27 @@ trait NumericTypes {
     )
 
   /**
-    * Implicit conversion from [[scala.Int]] to [[LONG]].
+    * Implicit conversion from [[scala.Int]] to [[astLong]].
     */
-  implicit def longToBabelLong(typ: Long.type): NumericType = LONG
+  implicit def longToBabelLong(typ: Long.type): NumericType = astLong
 
   /**
     * Represents an integer with an arbitrary precision.
     */
-  private[types] val BIG_INT: NumericType =
+  private[types] val astBigInt: NumericType =
     NumericType(
       maxNumberDecimals(0)
     )
 
   /**
-    * Implicit conversion from [[scala.BigInt]] to [[BIG_INT]].
+    * Implicit conversion from [[scala.BigInt]] to [[astBigInt]].
     */
-  implicit def bigIntToBabelBigInt(typ: BigInt.type): NumericType = BIG_INT
+  implicit def bigIntToBabelBigInt(typ: BigInt.type): NumericType = astBigInt
 
   /**
     * Represents a 32 bit float value on the Babel AST.
     */
-  private[types] val FLOAT: NumericType = {
+  private[types] val astFloat: NumericType = {
     @inline val maxNumberDecimalsInScalaFloat = 8
     NumericType(
       minValue(Float.MinValue),
@@ -96,12 +96,12 @@ trait NumericTypes {
   /**
     * Implicit conversion from [[scala.Float]] to [[Float]].
     */
-  implicit def floatToBabelFloat(typ: Float.type): NumericType = FLOAT
+  implicit def floatToBabelFloat(typ: Float.type): NumericType = astFloat
 
   /**
     * Represents a 64 bit float value on the Babel AST.
     */
-  private[types] val DOUBLE: NumericType = {
+  private[types] val astDouble: NumericType = {
     @inline val maxNumberDecimalsInScalaDouble = 16
     NumericType(
       minValue(Double.MinValue),
@@ -111,18 +111,18 @@ trait NumericTypes {
   }
 
   /**
-    * Implicit conversion from [[scala.Double]] to [[DOUBLE]].
+    * Implicit conversion from [[scala.Double]] to [[astDouble]].
     */
-  implicit def doubleToBabelDouble(typ: Double.type): NumericType = DOUBLE
+  implicit def doubleToBabelDouble(typ: Double.type): NumericType = astDouble
 
   /**
     * Represents an arbitrary precision float value on the Babel AST.
     */
-  private[types] val BIG_DECIMAL: NumericType = NumericType()
+  private[types] val astBigDecimal: NumericType = NumericType()
 
   /**
-    * Implicit conversion from [[scala.BigDecimal]] to [[BIG_DECIMAL]].
+    * Implicit conversion from [[scala.BigDecimal]] to [[astBigDecimal]].
     */
-  implicit def bigDecimalToBabelBigDecimal(typ: BigDecimal.type): NumericType = BIG_DECIMAL
+  implicit def bigDecimalToBabelBigDecimal(typ: BigDecimal.type): NumericType = astBigDecimal
 
 }
--- types/src/test/scala/types/primitives/numeric/NumericTypeSuites.scala
+++ types/src/test/scala/types/primitives/numeric/NumericTypeSuites.scala
@@ -12,35 +12,35 @@ class NumericTypeSuites extends Specification {
 
     "Perform automatically with 'scala.Byte'" in {
       val value: NumericType = Byte
-      value must beEqualTo(BYTE)
+      value must beEqualTo(astByte)
     }
     "Perform automatically with 'scala.Short'" in {
       val value: NumericType = Short
-      value must beEqualTo(SHORT)
+      value must beEqualTo(astShort)
     }
     "Perform automatically with 'scala.Int'" in {
       val value: NumericType = Int
-      value must beEqualTo(INTEGER)
+      value must beEqualTo(astInt)
     }
     "Perform automatically with 'scala.Long'" in {
       val value: NumericType = Long
-      value must beEqualTo(LONG)
+      value must beEqualTo(astLong)
     }
     "Perform automatically with 'scala.BigInt'" in {
       val value: NumericType = BigInt
-      value must beEqualTo(BIG_INT)
+      value must beEqualTo(astBigInt)
     }
     "Perform automatically with 'scala.Float" in {
       val value: NumericType = Float
-      value must beEqualTo(FLOAT)
+      value must beEqualTo(astFloat)
     }
     "Perform automatically with 'scala.Double'" in {
       val value: NumericType = Double
-      value must beEqualTo(DOUBLE)
+      value must beEqualTo(astDouble)
     }
     "Perform automatically with 'scala.BigDecimal" in {
       val value: NumericType = BigDecimal
-      value must beEqualTo(BIG_DECIMAL)
+      value must beEqualTo(astBigDecimal)
     }
   }
 }
