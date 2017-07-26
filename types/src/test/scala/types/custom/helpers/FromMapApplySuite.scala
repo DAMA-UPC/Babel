2de91c98032f15f8f5937e36ed954fe6cac44775
Xavier Fernández Salas <xavier.fernandez.salas@gmail.com>
2017-07-26 09:54:33 +0200

    Remove unecessary Scalacheck checkings on 'FromMapApply' macro suite

--- types/src/test/scala/types/custom/helpers/FromMapApplySuite.scala
+++ types/src/test/scala/types/custom/helpers/FromMapApplySuite.scala
@@ -1,39 +1,28 @@
 package types.custom.helpers
 
-import org.specs2.ScalaCheck
 import org.specs2.mutable.Specification
 
 /**
   * Test the macro [[FromMapApply]].
   */
-class FromMapApplySuite extends Specification with ScalaCheck {
+class FromMapApplySuite extends Specification {
 
   "Macro annotation expansion" should {
     "work with single parameter classes" in {
+      val expectedValue: Int = 5
       @FromMapApply case class Test(value: Int)
-      prop {
-        (expectedValue: Int) =>
-          Test.apply(Map("value" -> expectedValue)) must beEqualTo(Test(expectedValue))
-      }
+      Test.apply(Map("value" -> expectedValue)) must beEqualTo(Test(expectedValue))
     }
     "work with multiple parameter classes" in {
+      val intValue: Int = 5
+      val stringValue: String = "Hello"
+      val floatValue: Float = 5.0f
       @FromMapApply case class Test(intValue: Int, stringValue: String, floatValue: Float)
-      prop {
-        (intValue: Int, stringValue: String, floatValue: Float) =>
-          Test(
-            intValue = intValue,
-            stringValue = stringValue,
-            floatValue = floatValue
-          ) must beEqualTo(
-            Test.apply(
-              Map(
-                "intValue" -> intValue,
-                "stringValue" -> stringValue,
-                "floatValue" -> floatValue
-              )
-            )
-          )
-      }
+      Test(intValue = intValue, stringValue = stringValue, floatValue = floatValue) must beEqualTo(
+        Test.apply(
+          Map("intValue" -> intValue, "stringValue" -> stringValue, "floatValue" -> floatValue)
+        )
+      )
     }
   }
 }
